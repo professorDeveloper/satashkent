fcbee7fe34cfc8af4aa304ef70190060968b0a61
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2026-05-19 09:45:29 +0500

    ready to publish

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,15 +1,11 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class AppConstants {
   AppConstants._();
 
   static String get baseUrl => 'https://api.satashkent.uz/user-api';
 
+  static String get devLogin => "saikou";
 
-  static String? get devLogin => dotenv.maybeGet('DEV_LOGIN') ?? "saikou";
-
-  static String? get devPassword =>
-      dotenv.maybeGet('DEV_PASSWORD') ?? "_bujR2NQB7i6L.3";
+  static String get devPassword => "_bujR2NQB7i6L.3";
 
   static const String authBox = 'auth_box';
   static const String settingsBox = 'settings_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
@@ -13,9 +12,7 @@ import 'features/auth/presentation/bloc/auth_event.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  try {
-    await dotenv.load(fileName: '.env');
-  } catch (_) {}
+
   await Hive.initFlutter();
   await Hive.openBox(AppConstants.authBox);
   await Hive.openBox(AppConstants.settingsBox);
